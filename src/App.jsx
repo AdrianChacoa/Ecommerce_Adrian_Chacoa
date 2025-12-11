151b835ae4412df198cd3ed284d1ed69d6dba0e7
Adrian Chacoa <adrian30santana@gmail.com>
2025-12-10 23:19:04 -0300

    Primer commit del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/Header";
+import { ItemDetailContainer } from "./components/ItemDetailContainer/ItemDetailContainer";
+import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
+import { CartProvider } from "./context/CartContext/CartProvider";
+import { Cart } from "./components/Cart/Cart";
+import { ProductFormContainer } from "./components/adminComponents/ProductFormContainer/ProductFormContainer";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <CartProvider>
+          <Header />
+          <Routes>
+            <Route
+              path="/"
+              element={<ItemListContainer titulo={"CasePRO"} />}
+            />
+            <Route
+              path="/category/:category"
+              element={<ItemListContainer titulo={"CasePRO"} />}
+            />
+            <Route path="/detail/:id" element={<ItemDetailContainer />} />
+            <Route path="/carrito" element={<Cart />} />
+            <Route path="/admin" element={<ProductFormContainer />} />
+          </Routes>
+          <Footer />
+        </CartProvider>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
